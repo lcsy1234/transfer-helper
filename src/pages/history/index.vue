d48b345269d9ab302df3c071ca127ff35d395e52
lcsy1234 <xiayuling@yeez.tech>
2024-01-07 21:33:01 +0800

    init

---
+++ src/pages/history/index.vue
@@ -0,0 +1,8 @@
+<template>
+      <div>history</div>
+
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    index
+    <div class="chatInputOut">
+      <input type="text" class="chatInput" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style>
+.chatInputOut {
+  position: fixed;
+  bottom: 0;
+  background-color: #d2ecf9;
+  width: 100vw;
+  height: 40vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.chatInputOut {
+  width: 80%;
+  height: 80%;
+  border: 1px solid #1891ac;
+  /* box-shadow: ; */
+}
+</style>
